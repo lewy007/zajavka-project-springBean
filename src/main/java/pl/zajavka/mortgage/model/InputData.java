820f2461a5a995ef5bc6015f8b5a036b85109d73
Piotrek DELL 7480 <Piotrek DELL 7480@gmail.com>
2023-05-21 17:17:36 +0200

    DELL 7480 - projekt kalkulator hipoteczny bez Spring IoC Container

---
+++ src/main/java/pl/zajavka/mortgage/model/InputData.java
@@ -0,0 +1,49 @@
+package pl.zajavka.mortgage.model;
+
+import lombok.With;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.util.Map;
+
+@With
+public record InputData(
+    LocalDate repaymentStartDate,
+    BigDecimal wiborPercent,
+    BigDecimal amount,
+    BigDecimal monthsDuration,
+    MortgageType rateType,
+    BigDecimal marginPercent,
+    BigDecimal overpaymentProvisionPercent,
+    BigDecimal overpaymentProvisionMonths,
+    BigDecimal overpaymentStartMonth,
+    Map<Integer, BigDecimal> overpaymentSchema,
+    String overpaymentReduceWay,
+    boolean mortgagePrintPayoffsSchedule,
+    Integer mortgageRateNumberToPrint
+) {
+
+    private static final BigDecimal PERCENT = new BigDecimal("100");
+
+    public BigDecimal wiborPercent() {
+        return wiborPercent.divide(PERCENT, 4, RoundingMode.HALF_UP);
+    }
+
+    public BigDecimal marginPercent() {
+        return marginPercent.divide(PERCENT, 4, RoundingMode.HALF_UP);
+    }
+
+    public BigDecimal overpaymentProvisionPercent() {
+        return overpaymentProvisionPercent.divide(PERCENT, 4, RoundingMode.HALF_UP);
+    }
+
+    public BigDecimal interestPercent() {
+        return marginPercent().add(wiborPercent());
+    }
+
+    public BigDecimal interestToDisplay() {
+        return wiborPercent().add(marginPercent());
+    }
+
+}
